26a3cd014082d674a1089fda9aa205f7f9249f5b
PapineauA <papineautaidan@gmail.com>
2022-11-13 12:03:34 -0800

    Navbar + style

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const { response } = require("express");
-
+const places = require("../models/places.js");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
@@ -23,7 +23,24 @@ router.get("/", (req, res) => {
 });
 
 router.get("/new", (req, res) => {
+  console.log(req.body);
   res.render("places/new");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 module.exports = router;
