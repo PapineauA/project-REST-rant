13842a295476a815384342e83241cefab1149995
PapineauA <papineautaidan@gmail.com>
2022-11-20 09:54:34 -0800

    Adding mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,46 +1,61 @@
-const { response } = require("express");
-const places = require("../models/places.js");
 const router = require("express").Router();
+const db = require("../models");
 
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/thaiphoto.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/coffeecat.jpeg",
-    },
-  ];
-  res.render("places/index", { places });
-});
-
-router.get("/new", (req, res) => {
-  console.log(req.body);
-  res.render("places/new");
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = "http://placekitten.com/400/400";
-  }
-  if (!req.body.city) {
-    req.body.city = "Anytown";
-  }
-  if (!req.body.state) {
-    req.body.state = "USA";
-  }
-  places.push(req.body);
-  res.redirect("/places");
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+router.get("/:id", (req, res) => {
+  res.send("GET /places/:id stub");
+});
+
+router.put("/:id", (req, res) => {
+  res.send("PUT /places/:id stub");
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("DELETE /places/:id stub");
+});
+
+router.get("/:id/edit", (req, res) => {
+  res.send("GET edit form stub");
+});
+
+router.post("/:id/rant", (req, res) => {
+  res.send("GET /places/:id/rant stub");
+});
+
+router.delete("/:id/rant/:rantId", (req, res) => {
+  res.send("GET /places/:id/rant/:rantId stub");
 });
 
 module.exports = router;
