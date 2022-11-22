82aed0b9dae625dc5d8dd0c5acc05b1ceb6002c7
PapineauA <papineautaidan@gmail.com>
2022-11-22 15:16:25 -0800

    Seeding data, validations, and helper methods.

--- views/show.jsx
+++ views/show.jsx
@@ -8,17 +8,25 @@ function show (data) {
             <div>
               <h1>{ data.place.name }</h1>
             </div>
-            <a href = {`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
-            <form method = "POST" action= {`/places/${data.id}?_method=DELETE`}> 
-              <button type="submit" className="btn btn-danger">Delete</button>
-            </form>
+            <div>
+              <h3>Located in {data.place.city}, {data.place.state}</h3>
+            </div>
             <div>
               <h2>Ratings!</h2>
               <p>Not rated</p>
             </div>
             <div>
+              <h2>Description</h2>
+              <h3>{data.place.showEstablished()}</h3>
+              <h4>Serving {data.place.cuisines}</h4>
+            </div>
+            <a href = {`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
+            <form method = "POST" action= {`/places/${data.id}?_method=DELETE`}> 
+              <button type="submit" className="btn btn-danger">Delete</button>
+            </form>
+            <div>
               <h2>Comments</h2>
-              <p>Stuff</p>
+              <p>No comments yet!</p>
             </div>
           </main>
         </Def>
